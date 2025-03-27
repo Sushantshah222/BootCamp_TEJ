5ff4ac79632bfe8ab5fe95795807773af8e0357d
Sushant Shah <sushants@Sushants-MacBook-Air.local>
2025-03-27 22:29:55 +0545

    problem set started

--- bootcamp-prep-main/pre-work-workshops/05-functions/04-my-mnemonic/my-mnemonic.js
+++ bootcamp-prep-main/pre-work-workshops/05-functions/04-my-mnemonic/my-mnemonic.js
@@ -2,10 +2,49 @@
 
 // Self Solution I
 // Incomplete
-const myMnemonic = (a,b,c,d) => {
+// const myMnemonic = (a,b,c,d) => {
 
-    var word = a[0] + b[0] + c[0] + d[0];
-    return word;
-}
+//     let word = a[0] + b[0] + c[0] + d[0];
+//     return word;
+// }
 
-console.log(myMnemonic('Hello','Sushant','Shah','Namaste'));
+// console.log(myMnemonic('Hello','Sushant','Shah','Namaste'));
+
+
+// watched solution I
+// const myMnemonic =(firstWord,secondWord,thirdWord,fourthWord) =>{
+//     let menomic = '';
+
+//     if (firstWord !== undefined){
+//         menomic = menomic + firstWord[0]
+//     };
+    
+//     if (secondWord !== undefined) menomic += secondWord[0];
+//     if (thirdWord !== undefined) menomic += thirdWord[0];
+//     if (fourthWord !== undefined) menomic += fourthWord[0];
+
+//     return menomic;
+// };
+
+// // console.log(myMnemonic("Sushant", "Shah", "Nepal", "Dharan"))
+// // console.log(myMnemonic("Sushant", "Shah"))
+
+
+
+// solution II
+// rest operator,turning all operator into array
+
+const myMnemonic=(...words)=>{
+    memonic =" ";
+
+    for (let i=0; i < words.length; i++){
+        const currentWord = words[i];
+        const currentCharacter = currentWord[0];
+
+        memonic += currentCharacter;
+
+    }
+    return memonic;
+};
+
+console.log(myMnemonic("Sushant","Shah","Pi Innovations","Biratnagar"));
---
+++ bootcamp-prep-main/pre-work-workshops/06-problem-set/01-greeting/greeting.js
@@ -0,0 +1,33 @@
+// YOUR CODE BELOW
+
+
+// Self Solution
+const greeting = (name)=> {
+    if (name){
+        return `Hello ${name}!`;
+    }
+
+    else{
+        return `Hello!`;
+    };
+};
+
+console.log(greeting("Sushant"));
+console.log(greeting());
+
+
+
+
+// // watched Solution II
+// const greeting =(name) =>{
+//     if(typeof(name) === 'string' && name.length>0){
+//         return `Hello ${name}!`;
+//     }
+
+//     else{
+//         return `Hello!`;
+//     };
+// };
+
+// console.log(greeting("Sushant"));
+// console.log(greeting());
